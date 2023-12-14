68305f227ea5f8e79eca909fcb60a3f1437f9a25
Kevin Stierhoff <kevins@ucsc.edu>
2023-12-14 11:06:40 -0800

    Improvements to data processing. Created a function for extracting file data. Now will be able to iterate over all files as needed.

--- code/read_tv80.R
+++ code/read_tv80.R
@@ -5,6 +5,8 @@ if (!require("pacman")) install.packages("pacman")
 
 # Install and load required packages from CRAN ---------------------------------
 pacman::p_load(tidyverse, lubridate, fs, here, mapproj)
+
+# Plotting preferences
 theme_set(theme_bw())
 
 dat.files <- fs::dir_ls(here("data/TV80"), 
@@ -15,45 +17,43 @@ hdr.files <- fs::dir_ls(here("data/TV80"),
                         recurse = TRUE,
                         regexp = "*.header_names.csv")
 
-# dat.tmp <- read.table(dat.files[5], header = TRUE, sep = ";") %>% 
-#   mutate(datetime = ymd_hms(DateTime))
+read_tv80_msg <- function(file) {
+  # Convert file to path to extract file name
+  file.name <- path_file(as_fs_path(file))
+  
+  # Read and format CSV file data
+  df <- read_delim(file, delim = ";", name_repair = "minimal", lazy = FALSE) %>%
+    select(which(!duplicated(names(.)))) %>%
+    mutate(VES_Latitude = as.numeric(str_sub(VES_Latitude,1,2)) +
+             as.numeric(str_sub(VES_Latitude,4,11))/60,
+           VES_Longitude = as.numeric(str_sub(VES_Longitude,1,3)) +
+             as.numeric(str_sub(VES_Longitude,5,12))/60) %>%
+    mutate_if(is.character, as.numeric) %>%
+    mutate(datetime = ymd_hms(DateTime),
+           file = file.name)
+  
+  return(df)
+}
 
-dat.tmp <- read_delim(dat.files[12], delim = ";", name_repair = "minimal") 
+# Extract data
+dat <- read_tv80_msg(file = dat.files[12])
 
-dat <- dat.tmp[, which(!duplicated(names(dat.tmp)))] %>% 
-  mutate(datetime = ymd_hms(DateTime)) %>% 
-  mutate(lat = as.numeric(str_sub(VES_Latitude,1,2)) +
-           as.numeric(str_sub(VES_Latitude,4,11))/60,
-         long = as.numeric(str_sub(VES_Longitude,1,3)) +
-           as.numeric(str_sub(VES_Longitude,5,12))/60)
-
+# Plot door and trawl depths
 ggplot() + 
-  geom_line(data = dat, aes(datetime, -as.numeric(TWL_Depth)), colour = "blue") +
-  geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-  geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Std)), colour = "green") +
-  scale_y_continuous(name = "Depth (m)", limits = c(-400,0))
+  geom_line(data = dat, aes(datetime, -TWL_Depth), colour = "blue") +
+  geom_line(data = dat, aes(datetime, -DOR_Depth_Prt), colour = "red") +
+  geom_line(data = dat, aes(datetime, -DOR_Depth_Std), colour = "green") +
+  scale_y_continuous(name = "Depth (m)")
 
-# ggplot(dat, aes(long, lat, colour = VES_Speed)) + geom_point() + coord_map()
-# 
-# ggplot() + 
-#   # geom_line(data = dat, aes(datetime, -as.numeric(TWL_Depth))) +
-#   # geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-#   geom_line(data = dat, aes(datetime, as.numeric(TWL_Geometry_Prt)), colour = "red") +
-#   geom_line(data = dat, aes(datetime, as.numeric(TWL_Geometry_Std)), colour = "blue")
-# 
-# ggplot() + 
-#   # geom_line(data = dat, aes(datetime, -as.numeric(TWL_Depth))) +
-#   # geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-#   geom_histogram(data = dat, aes(as.numeric(TWL_Geometry_Prt)), fill = "red") +
-#   geom_histogram(data = dat, aes(as.numeric(TWL_Geometry_Std)), fill = "blue")  
-# 
-# ggplot() + 
-#   # geom_line(data = dat, aes(datetime, -as.numeric(TWL_Depth))) +
-#   # geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-#   geom_line(data = dat, aes(datetime, as.numeric(DOR_Spread)), colour = "red") 
-# 
-# ggplot() + 
-#   # geom_line(data = dat, aes(datetime, -as.numeric(TWL_Depth))) +
-#   # geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-#   geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Prt)), colour = "red") +
-#   geom_line(data = dat, aes(datetime, -as.numeric(DOR_Depth_Std)), colour = "blue")
+# Plot trawl path
+ggplot(dat, aes(VES_Longitude, VES_Latitude, colour = VES_Speed)) + geom_point() + coord_map()
+
+# Plot trawl geometry
+ggplot() +
+  geom_line(data = dat, aes(datetime, TWL_Geometry_Prt), colour = "red") +
+  geom_line(data = dat, aes(datetime, TWL_Geometry_Std), colour = "blue")
+
+# Plot door spread
+ggplot() +
+  geom_line(data = dat, aes(datetime, DOR_Spread), colour = "red")
+
